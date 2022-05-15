26adfe99be9b7232d7315fd890cd2e7ba6e89a24
Irina Aseeva <irina.v.aseeva@gmail.com>
2022-05-15 21:45:31 +0300

    Module 2/ 10/ Forms 2

--- src/components/page/userPage/userPage.jsx
+++ src/components/page/userPage/userPage.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import api from '../api'
-import QualitiesList from './qualitiesList'
-import { Link } from 'react-router-dom'
+import api from '../../../api'
+import Qualities from '../../ui/qualities/qualitiesList'
+import { Link, useParams } from 'react-router-dom'
 
-const UserPage = ({ id }) => {
+const UserPage = () => {
+    const params = useParams()
+    const { userId } = params
+
     const [userData, setUserData] = useState()
 
     useEffect(() => {
-        api.users.getById(id).then((data) => setUserData(data))
+        api.users.getById(userId).then((data) => setUserData(data))
     }, [])
 
     if (userData) {
@@ -16,10 +19,10 @@ const UserPage = ({ id }) => {
             <>
                 <h1>{userData.name}</h1>
                 <h2>Профессия: <span>{userData.profession.name}</span></h2><br />
-                <div><QualitiesList qualities={userData.qualities} /></div><br />
+                <div><Qualities qualities={userData.qualities} /></div><br />
                 <strong>completedMeetings: {userData.completedMeetings}</strong><br /><br />
                 <h3>Rate: <span>{userData.rate}</span></h3>
-                <Link to="/users"><span className="btn btn-primary mt-2">Все пользователи</span></Link>
+                <Link to={'/users/' + userId + '/edit'}><span className="btn btn-primary mt-2">Изменить</span></Link>
             </>
         )
     }
